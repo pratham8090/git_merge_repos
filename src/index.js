54f615650bdfe480440d24ec13c05179aa3dbd02
pratham8090 <pratham@interbitsolutions.com>
2022-12-23 19:30:50 +0530

    promise added

--- src/index.js
+++ src/index.js
@@ -21,20 +21,20 @@ app.post("/test", async function (req, res) {
 
   let commands = [];
   commands.push("git clone " + req.body.repo_all);
-  commands.push("cd " + fileNameRepoAll);
-  commands.push("git clone " + req.body.repo_1);
-  commands.push("git clone " + req.body.repo_2);
+  // commands.push("cd " + fileNameRepoAll);
+  commands.push("git clone " + req.body.repo_1 + " "+ fileNameRepoAll +"/repo1");
+  commands.push("git clone " + req.body.repo_2 + " "+ fileNameRepoAll +"/repo2");
 
-  commands.push("cd " + fileNameRepo1);
-  commands.push("rm -r .git");
-  commands.push("cd ..");
+  // commands.push("cd " + fileNameRepo1);
+  commands.push("rmdir repo1/.git");
+  // commands.push("cd ..");
 
-  commands.push("cd " + fileNameRepo2);
-  commands.push("rm -r .git");
-  commands.push("cd ..");
+  // commands.push("cd " + fileNameRepo2);
+  commands.push("rmdir repo2/.git");
+  // commands.push("cd ..");
 
-  commands.push('git commit -m "merge rep-frontend"');
-  commands.push('git push');
+  // commands.push('git commit -m "merge rep-frontend"');
+  // commands.push('git push');
 
   // console.log(commands);
   // return;
@@ -50,19 +50,24 @@ app.post("/test", async function (req, res) {
   //   }
   //   console.log(`stdout: ${stdout}`);
   // });
-
+  
   for (let i = 0; i < commands.length; i++) {
-    console.log('Running ['+ commands[i] +']');
-    await exec(commands[i], (error, stdout, stderr) => {
-      if (error) {
-        console.log(`Error while running ${commands[i]}: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.log(`Stderr while running ${commands[i]}: ${stderr}`);
-        return;
-      }
-      console.log(`Stdout while running ${commands[i]}: ${stdout}`);
+      console.log("inLoop")
+    await new Promise((resolve, reject) => {
+      console.log("inFunction")
+      console.log('Running ['+ commands[i] +']');
+      exec(commands[i], (error, stdout, stderr) => {
+        if (error) {
+          console.log(`Error while running ${commands[i]}: ${error.message}`);
+          reject(error)
+        }
+        if (stderr) {
+          console.log(`Stderr while running ${commands[i]}: ${stderr}`);
+          resolve(stderr);
+        }
+        console.log(`Stdout while running ${commands[i]}: ${stdout}`);
+        resolve(stdout);
+      });
     });
   }
 });
